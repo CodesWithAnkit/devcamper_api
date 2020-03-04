e574b8ef03c9b8569c6ac745418c62fc6eb43d3e
Ankit Kumar Sharma <43175867+CodesWithAnkit@users.noreply.github.com>
2020-03-04 16:19:34 +0530

    Created first post to db

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -1,3 +1,5 @@
+const Bootcamp = require('../models/Bootcamp');
+
 // @desc  get the all bootcapms
 // routes GET  /api/v1/bootcamps
 // @access public
@@ -18,8 +20,17 @@ exports.getBootcamp = (req, res, next) => {
 // routes POST  /api/v1/bootcamps
 // @access Private
 
-exports.createBootcamp = (req, res, next) => {
-  res.status(200).json({ success: true, msg: 'Create new bootcamps' });
+exports.createBootcamp = async (req, res, next) => {
+  try {
+    const bootcamp = await Bootcamp.create(req.body);
+
+    res.status(201).json({
+      success: true,
+      data: bootcamp
+    });
+  } catch (error) {
+    res.status(400).json({ success: false });
+  }
 };
 
 // @desc  Update new bootcapms
